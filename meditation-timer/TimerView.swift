6790d6d9fda60fcaccde1a187aa74b0b06b9a3ac
Harrison Borges <hborges005@gmail.com>
2023-08-16 16:58:51 -0600

    refactor timer logic into model

--- meditation-timer/TimerView.swift
+++ meditation-timer/TimerView.swift
@@ -1,105 +1,55 @@
 import SwiftUI
 
 struct TimerView: View {
-	@State private var meditationTime: Int = 3 // Tracks the length of the timer a user chooses
-	@State private var timerProgress: CGFloat = 0 // Used by the progress indicator
-	@State private var remainingTime: Int = 180 // Remaining time in seconds, used to display the time to the user
-
-	@State private var timer: Timer? = nil
-	@State private var isRunning: Bool = false
+	@EnvironmentObject var timer: TimerModel
 
 	var body: some View {
 		VStack {
 			ZStack {
-			ProgressIndicator(progress: timerProgress)
-				.frame(width: 300, height: 300)
+				ProgressIndicator(progress: timer.timerProgress)
+					.frame(width: 300, height: 300)
 
-			Text(isRunning ? "\(remainingTime / 60) min \(remainingTime % 60) sec" : "\(meditationTime) min")
-				.font(.title)
+				Text(timer.isRunning ? "\(timer.remainingTime / 60) min \(timer.remainingTime % 60) sec" : "\(timer.meditationTime) min")
+					.font(.title)
 			}
 
 			HStack {
 				// Decrement button
-				Button(action: decrementTime, label: {
+				Button(action: timer.decrementTime, label: {
 					Image(systemName: "minus")
 						.font(.system(size: 18))
 						.frame(width: 32, height: 32)
 						.background(Circle().fill(Color.gray.opacity(0.2)))
 				})
-				.disabled(isRunning)
+				.disabled(timer.isRunning)
 
+				// Play/pause button
 				Button(action: {
-					if isRunning {
-						stopTimer()
+					if timer.isRunning {
+						timer.stopTimer()
 					} else {
-						startTimer()
+						timer.startTimer()
 					}
 				}, label: {
-					Image(systemName: isRunning ? "pause" : "play")
+					Image(systemName: timer.isRunning ? "pause" : "play")
 						.font(.system(size: 24))
 						.frame(width: 44, height: 44)
 						.background(Circle().fill(Color.gray.opacity(0.2)))
 				})
 
 				// Increment button
-				Button(action: incrementTime, label: {
+				Button(action: timer.incrementTime, label: {
 					Image(systemName: "plus")
 						.font(.system(size: 18))
 						.frame(width: 32, height: 32)
 						.background(Circle().fill(Color.gray.opacity(0.2)))
 				})
-				.disabled(isRunning)
+				.disabled(timer.isRunning)
 			}
-			.padding(.horizontal, 20)
+			.padding(.horizontal, 40)
 		}
 	}
 
-	private func startTimer() {
-		stopTimer() // Ensure any existing timer is stopped
-		remainingTime = meditationTime * 60
-		timerProgress = 1 // Setting the initial value for the timer's progress
-		timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
-			if remainingTime > 0 {
-				remainingTime -= 1
-				timerProgress = CGFloat(remainingTime) / CGFloat(meditationTime * 60)
-			} else {
-				stopTimer()
-			}
-		}
-		isRunning = true
-	}
-
-
-	private func stopTimer() {
-		timer?.invalidate()
-		timer = nil
-		isRunning = false
-		timerProgress = 0
-	}
-
-	private func incrementTime() {
-		switch meditationTime {
-			case 3:
-				meditationTime = 5
-			case 4..<30:
-				meditationTime += 5
-			case 30..<60:
-				meditationTime += 15
-			default:
-				break
-		}
-	}
-
-	private func decrementTime() {
-		switch meditationTime {
-			case 3,5:
-				meditationTime = 3
-			case 6...30:
-				meditationTime -= 5
-			default:
-				meditationTime -= 15
-		}
-	}
 }
 
 
--- meditation-timer/meditation_timerApp.swift
+++ meditation-timer/meditation_timerApp.swift
@@ -6,12 +6,32 @@
 //
 
 import SwiftUI
+import BackgroundTasks
 
 @main
 struct MeditationTimerApp: App {
+	@Environment(\.scenePhase) private var scenePhase
+	@StateObject private var timerModel = TimerModel()
+
+	init() {
+		BGTaskScheduler.shared.register(
+			forTaskWithIdentifier: "com.yourapp.task",
+			using: nil
+		) { task in
+			// Play sound for finished meditation.
+			// Will need to pull the timer duration somehow
+		}
+	}
+
 	var body: some Scene {
 		WindowGroup {
 			TimerView()
+				.environmentObject(timerModel)
+		}
+		.onChange(of: scenePhase) { phase in
+			if phase == .background {
+				// Schedule the background task
+			}
 		}
 	}
 }
