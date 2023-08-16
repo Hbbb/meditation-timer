98477bf21b0df3288d5147f78428da1e203c0141
Harrison Borges <hborges005@gmail.com>
2023-08-16 10:03:53 -0600

    Initial Commit

---
+++ meditation-timer/meditation_timerApp.swift
@@ -0,0 +1,17 @@
+//
+//  meditation_timerApp.swift
+//  meditation-timer
+//
+//  Created by Harrison Borges on 8/16/23.
+//
+
+import SwiftUI
+
+@main
+struct meditation_timerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
