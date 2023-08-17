5799382c5ff84938c5e14ed2da16956050028ea5
Harrison Borges <hborges005@gmail.com>
2023-08-16 22:40:22 -0600

    Create rounded rectangle background view

---
+++ meditation-timer/RoundedRectangleView.swift
@@ -0,0 +1,64 @@
+//
+//  RoundedRectangleView.swift
+//  meditation-timer
+//
+//  Created by Harrison Borges on 8/16/23.
+//
+
+import SwiftUI
+
+struct RoundedRectangleView<Content: View>: View {
+	let content: Content
+
+	init(@ViewBuilder content: () -> Content) {
+		self.content = content()
+	}
+
+	var body: some View {
+		GeometryReader { geometry in
+			VStack {
+				Spacer()
+				ZStack(alignment: .bottom) {
+					Color.gray
+						.frame(width: geometry.size.width, height: geometry.size.height * 0.60)
+						.cornerRadius(150, corners: [.topLeft, .topRight])
+
+					content
+						.padding()
+				}
+			}
+		}
+		.edgesIgnoringSafeArea(.bottom)
+	}
+}
+
+// Custom corner radius implementation
+extension View {
+	func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+		clipShape(RoundedCorner(radius: radius, corners: corners))
+	}
+}
+
+struct RoundedCorner: Shape {
+	var radius: CGFloat = .infinity
+	var corners: UIRectCorner = .allCorners
+
+	func path(in rect: CGRect) -> Path {
+		let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+		return Path(path.cgPath)
+	}
+}
+
+
+struct RoundedRectangleView_Previews: PreviewProvider {
+	static var previews: some View {
+		RoundedRectangleView {
+			Text("Your Cooooooontent Here")
+				.padding()
+				.background(Color.white)
+				.cornerRadius(10)
+		}
+	}
+}
+
+
