cbb474c87bcc2b6a9f721bab6a68a3ae9d00c3a4
Harrison Borges <hborges005@gmail.com>
2023-08-25 21:02:05 -0600

    remove tabbed nav

--- meditation-timer/App.swift
+++ meditation-timer/App.swift
@@ -12,29 +12,20 @@ struct MeditationTimerApp: App {
 	@Environment(\.scenePhase) private var scenePhase
 	@StateObject private var timerModel = TimerModel()
 
-//	init() {
-//		BGTaskScheduler.shared.register(
-//			forTaskWithIdentifier: "com.yourapp.task",
-//			using: nil
-//		) { task in
-//			// Play sound for finished meditation.
-//			// Will need to pull the timer duration somehow
-//		}
-//	}
+	//	init() {
+	//		BGTaskScheduler.shared.register(
+	//			forTaskWithIdentifier: "com.yourapp.task",
+	//			using: nil
+	//		) { task in
+	//			// Play sound for finished meditation.
+	//			// Will need to pull the timer duration somehow
+	//		}
+	//	}
 
 	var body: some Scene {
 		WindowGroup {
-			TabView {
-				TimerView()
-					.tabItem {
-						Label("Timer", systemImage: "stopwatch")
-					}
-
-				HistoryView()
-					.tabItem {
-						Label("History", systemImage: "list.clipboard")
-					}
-			}.environmentObject(timerModel)
+			TimerView()
+				.environmentObject(timerModel)
 		}
 		.onChange(of: scenePhase) { phase in
 			if phase == .background {
