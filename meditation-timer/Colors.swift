064bb4045e7d9051b13d9bd3399b5c8eb4c264d4
Harrison Borges <hborges005@gmail.com>
2023-08-25 20:53:12 -0600

    adds colors

---
+++ meditation-timer/Colors.swift
@@ -0,0 +1,14 @@
+//
+//  Colors.swift
+//  meditation-timer
+//
+//  Created by Harrison Borges on 8/23/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct Colors {
+	static let primary = Color("primary")
+	static let secondary = Color("secondary")
+}
