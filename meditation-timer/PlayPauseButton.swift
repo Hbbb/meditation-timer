aff29ce3188d17d5c7369d45f64b165b62563913
Harrison Borges <hborges005@gmail.com>
2023-08-26 10:55:57 -0600

    animate play/pause

--- meditation-timer/PlayPauseButton.swift
+++ meditation-timer/PlayPauseButton.swift
@@ -9,23 +9,35 @@ import SwiftUI
 
 struct PlayPauseButton: View {
 	@EnvironmentObject var timer: TimerModel
+	@State private var rotation: Double = 0
 
 	private var icon: String {
 		timer.isRunning ? "stop.fill" : "play.fill"
 	}
 
 	var body: some View {
-		Button(action: {
-			if timer.isRunning {
-				timer.stop()
-			} else {
-				timer.start()
-			}
-		}, label: {
+		ZStack {
+			Circle()
+				.fill(Colors.secondary)
+				.frame(width: 90, height: 90)
+				.onTapGesture {
+					withAnimation {
+						if timer.isRunning {
+							rotation = 0
+							timer.stop()
+						} else {
+							rotation += 90
+							timer.start()
+						}
+					}
+
+					UIImpactFeedbackGenerator(style: .light).impactOccurred()
+				}
 			Image(systemName: icon)
 				.font(.system(size: 24))
+				.rotationEffect(.degrees(rotation))
+				.transition(.identity)
 				.frame(width: 90, height: 90)
-				.background(Circle().fill(Colors.secondary))
-		})
+		}
 	}
 }
