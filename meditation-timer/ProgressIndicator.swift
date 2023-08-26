c44b0de07c01ac28838bbe63f48e6a77951dc846
Harrison Borges <hborges005@gmail.com>
2023-08-25 20:52:53 -0600

    adds circular progress indicator

--- meditation-timer/ProgressIndicator.swift
+++ meditation-timer/ProgressIndicator.swift
@@ -23,8 +23,8 @@ struct ProgressIndicator: View {
 											endAngle: .degrees(360),
 											clockwise: false)
 				}
-				.stroke(style: StrokeStyle(lineWidth: 5, lineCap: .round))
-				.foregroundColor(.gray)
+				.stroke(style: StrokeStyle(lineWidth: 7, lineCap: .round))
+				.foregroundColor(.clear)
 
 				// Foreground path for the progress
 				Path { path in
@@ -36,13 +36,34 @@ struct ProgressIndicator: View {
 											endAngle: .degrees(endAngle),
 											clockwise: false)
 				}
-				.stroke(style: StrokeStyle(lineWidth: 5, lineCap: .round))
-				.foregroundColor(.blue)
+				.stroke(style: StrokeStyle(lineWidth: 7, lineCap: .round))
+				.foregroundColor(Colors.secondary)
 			}
 			.rotationEffect(.degrees(45))
 		}
 	}
+}
 
+struct CircularProgressIndicator: View {
+	var progress: Double
+
+	var body: some View {
+		GeometryReader { geometry in
+			Path { path in
+				let radius = min(geometry.size.width, geometry.size.height) / 2
+				let startAngle = Angle(degrees: 0)
+				let endAngle = Angle(degrees: 360 * (1 - self.progress))
+
+				path.addArc(center: CGPoint(x: geometry.size.width / 2, y: geometry.size.height / 2),
+										radius: radius,
+										startAngle: startAngle,
+										endAngle: endAngle,
+										clockwise: true)
+			}
+			.stroke(Colors.secondary, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+			.rotationEffect(Angle(degrees: -90))
+		}
+	}
 }
 
 struct ProgressIndicator_Previews: PreviewProvider {
@@ -56,6 +77,16 @@ struct ProgressIndicator_Previews: PreviewProvider {
 	}
 }
 
+//struct CircularProgressIndicator_Previews: PreviewProvider {
+//	static var previews: some View {
+//		VStack {
+//			Spacer()
+//			CircularProgressIndicator(progress: 0.9)
+//			Spacer()
+//		}
+//		.frame(maxWidth: .infinity, maxHeight: .infinity)
+//	}
+//}
 
 
 
