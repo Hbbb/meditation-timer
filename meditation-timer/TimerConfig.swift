0db8dcfd898a7abefda592d9456f27d5f04567aa
Harrison Borges <hborges005@gmail.com>
2023-09-03 14:41:54 -0600

    fix timerconfig layout

--- meditation-timer/TimerConfig.swift
+++ meditation-timer/TimerConfig.swift
@@ -12,39 +12,88 @@ struct TimerConfig: View {
 
 	var body: some View {
 		VStack() {
-			Text("Meditation")
-			HStack {
-				Image(systemName: "minus")
-				Text("\(viewModel.timerDuration)")
-					.font(.system(size: 42))
-				Image(systemName: "plus")
-			}
 
-			Text("Warmup")
-			HStack {
-				Image(systemName: "minus")
-				Text("\(viewModel.timerDuration)")
-					.font(.system(size: 42))
-				Image(systemName: "plus")
+			VStack {
+				Text("Meditation")
+				HStack {
+					IncDecButton(.minus) {
+						viewModel.removeTime()
+					}
+
+					Text(viewModel.timerDuration.toMMSS())
+						.font(.system(size: 64))
+						.padding(.horizontal, 20)
+
+					IncDecButton(.plus) {
+						viewModel.addTime()
+					}
+				}
 			}
+			.padding(.vertical)
+
+			VStack {
+				Text("Warmup")
+				HStack {
+					IncDecButton(.minus) {
+
+					}
+
+					Text(viewModel.warmupDuration.toMMSS())
+						.font(.system(size: 64))
+						.padding(.horizontal, 20)
+
+					IncDecButton(.plus) {
+
+					}
+				}
+			}
+			.padding(.vertical)
+			.padding(.bottom, 100)
 
 			HStack {
 				Image(systemName: "speaker.wave.2.fill")
-				PlayPauseButton(icon: "play.fill") {
+
+				PlayPauseButton(icon: "play.circle") {
 					Logger.info("Start timer", context: .timerConfig)
 					viewModel.startTimer()
 				}
+				.padding(.horizontal, 80)
 
 				Image(systemName: "square.and.arrow.down.fill")
-			}
+			}.padding([.top, .bottom])
 		}
 		.frame(maxHeight: .infinity, alignment: .bottom)
 	}
 }
 
+enum IncDecControl: String {
+	case plus
+	case minus
+}
+
+struct IncDecButton: View {
+	let icon: IncDecControl
+	let action: (() -> Void)
+
+	init(_ control: IncDecControl, action: @escaping () -> Void) {
+		icon = control
+		self.action = action
+	}
+
+	var body: some View {
+		Image(systemName: icon.rawValue)
+			.font(.system(size: 32))
+			.onTapGesture {
+				self.action()
+			}
+	}
+}
+
 struct TimerConfig_Previews: PreviewProvider {
 	static var previews: some View {
-		TimerConfig()
-			.environmentObject(AppViewModel())
+		Group {
+			TimerConfig()
+				.environmentObject(AppViewModel())
+		}
 	}
 }
