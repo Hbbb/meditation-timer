63ca2bba14473cb452d61c0cc76424d7e830ecfc
Harrison Borges <hborges005@gmail.com>
2023-09-05 17:51:48 -0600

    rebuild content view

--- meditation-timer/ContentView.swift
+++ meditation-timer/ContentView.swift
@@ -11,44 +11,50 @@ struct ContentView: View {
 	@Environment(\.managedObjectContext) var moc
 
 	var body: some View {
-		TimerConfig()
-			.sheet(isPresented: $viewModel.timerIsRunning) {
+		switch viewModel.timerState {
+			case .config:
+				TimerConfig()
+			case .warmup:
 				VStack {
 					ZStack {
-						CircularProgressIndicator(progress: viewModel.timerProgress)
-							.frame(width: 300, height: 300)
-
-						ZStack {
-							Circle()
-								.fill(Colors.primary)
-								.frame(width: 225, height: 225)
-							Text(viewModel.timeRemaining.toMMSS())
-								.font(.custom("Varela Round", size: 52))
+						VStack {
+							Text("-\(viewModel.warmupTimeRemaining.toMMSS())")
+							Text("Warm Up")
 						}
+						CircularProgressIndicator(progress: viewModel.warmupProgress)
+							.frame(width: 225, height: 225)
 					}
-
-					PlayPauseButton(icon: "stop.circle") {
-						viewModel.stopTimer()
-					}
-					.padding(.vertical)
+					.padding(.bottom, 120)
+					Image(systemName: "forward.end.fill")
+						.font(.system(size: 40))
 				}
-			}
+			case .meditation:
+				VStack {
+					ZStack {
+						VStack {
+							Text(viewModel.timeRemaining.toMMSS())
+							Text("Meditation")
+						}
+						CircularProgressIndicator(progress: viewModel.timerProgress)
+							.frame(width: 225, height: 225)
+					}
+					.padding(.bottom, 120)
+					Image(systemName: "stop.fill")
+						.font(.system(size: 40))
+				}
+		}
 
 		// This is a hack I use to find real names of fonts. Don't even ask
-				.onAppear {
-					for family: String in UIFont.familyNames
-					{
-						print(family)
-						for names: String in UIFont.fontNames(forFamilyName: family)
-						{
-							print("== \(names)")
-						}
-					}
-				}
-	}
-
-	private func updateIdleTimer() {
-		UIApplication.shared.isIdleTimerDisabled = true
+		//				.onAppear {
+		//					for family: String in UIFont.familyNames
+		//					{
+		//						print(family)
+		//						for names: String in UIFont.fontNames(forFamilyName: family)
+		//						{
+		//							print("== \(names)")
+		//						}
+		//					}
+		//				}
 	}
 }
 
