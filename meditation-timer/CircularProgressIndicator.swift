2b586bff79343cf548f4719a381eb1f32fa0d90a
Harrison Borges <hborges005@gmail.com>
2023-09-05 17:51:28 -0600

    update circle progress indicator width

--- meditation-timer/CircularProgressIndicator.swift
+++ meditation-timer/CircularProgressIndicator.swift
@@ -16,14 +16,14 @@ struct CircularProgressIndicator: View {
 			Circle()
 				.stroke(
 					Colors.secondary.opacity(0.5),
-					lineWidth: 20
+					lineWidth: 15
 				)
 		Circle()
 			.trim(from: 1 - progress, to: 1)
 			.stroke(
 				Colors.secondary,
 				style: StrokeStyle(
-					lineWidth: 20,
+					lineWidth: 15,
 					lineCap: .round
 				)
 			)
@@ -36,8 +36,8 @@ struct CircularProgressIndicator: View {
 struct CircularProgressIndicator_Previews: PreviewProvider {
 	static var previews: some View {
 		VStack {
-			CircularProgressIndicator(progress: 0.80)
-				.frame(width: 200, height: 200)
+			CircularProgressIndicator(progress: 0.70)
+				.frame(width: 225, height: 225)
 		}
 		.frame(maxWidth: .infinity, maxHeight: .infinity)
 	}
