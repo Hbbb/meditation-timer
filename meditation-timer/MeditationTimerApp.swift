4200cd364fe6368135db0a8efad333a36e21705e
Harrison Borges <hborges005@gmail.com>
2023-09-06 13:00:35 -0600

    remove unused entities from environment

--- meditation-timer/MeditationTimerApp.swift
+++ meditation-timer/MeditationTimerApp.swift
@@ -11,9 +11,7 @@ import SwiftUI
 struct MeditationTimerApp: App {
 	@Environment(\.scenePhase) private var scenePhase
 
-	@StateObject private var viewModel = AppViewModel()
 	@StateObject private var dataController = DataController()
-	@StateObject private var alarmPlayer = AlarmPlayer()
 	@StateObject private var vm = MeditationViewModel()
 
 	private var backgroundTask = BackgroundTask()
@@ -31,32 +29,8 @@ struct MeditationTimerApp: App {
 	var body: some Scene {
 		WindowGroup {
 			ContentView()
-				.environmentObject(backgroundTask)
-				.environmentObject(alarmPlayer)
-				.environmentObject(viewModel)
 				.environmentObject(vm)
 				.environment(\.managedObjectContext, dataController.container.viewContext)
-				.onAppear {
-					viewModel.timerDidStart = {
-						alarmPlayer.playSound(soundName: "singing-bowl", volume: 100.0)
-
-						// TODO: This is a temporary hack. Once I get a proper track I won't need to cut this off
-						Thread.sleep(forTimeInterval: 5.0)
-						alarmPlayer.stopSound()
-					}
-
-					viewModel.timerDidComplete = {
-						alarmPlayer.playSound(soundName: "singing-bowl", volume: 100.0)
-
-						// TODO: This is a temporary hack. Once I get a proper track I won't need to cut this off
-						Thread.sleep(forTimeInterval: 5.0)
-						alarmPlayer.stopSound()
-
-						// Stop playing silent track after timer completes. The next time a user
-						// opens the app to start a new timer, the backgroundTask will resume
-						backgroundTask.stop()
-					}
-				}
 		}
 		.onChange(of: scenePhase) { phase in
 			switch(phase) {
@@ -65,33 +39,12 @@ struct MeditationTimerApp: App {
 					/**
 					 If there is an active timer:
 					 - Play silent .wav track which enables us to play a sound when the timer ends, even if the app is not in the foreground
-					 - Capture the time the user went to the background. This is used to compute the time difference when they re-open the app
 					 */
-					if viewModel.viewState != .config {
+					if vm.screenState != .setup {
 						backgroundTask.start()
-						viewModel.didGoToBackgroundAt = .now
 					}
 				case .active:
 					backgroundTask.stop()
-
-					// If there's not an active timer running, no need to handle this
-					if viewModel.viewState == .config {
-						return
-					}
-
-					if let backroundAt = viewModel.didGoToBackgroundAt {
-						viewModel.didGoToBackgroundAt = nil
-
-						let elapsedSeconds = Int(Date().timeIntervalSince(backroundAt))
-
-						// The timer is expired, take them back to the config screen
-						if elapsedSeconds > viewModel.timeRemaining {
-							viewModel.viewState = .config
-						} else {
-							viewModel.timeRemaining = viewModel.timeRemaining - elapsedSeconds
-						}
-					}
-
 				default: ()
 			}
 		}
