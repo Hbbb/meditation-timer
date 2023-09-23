4771247c30cb66a03cbf2d672a651752ede8b66d
Harrison Borges <hborges005@gmail.com>
2023-09-23 17:46:18 -0600

    add Complete button to completed view

--- meditation-timer/CompletedMeditation.swift
+++ meditation-timer/CompletedMeditation.swift
@@ -9,16 +9,31 @@ import SwiftUI
 
 struct CompletedMeditation: View {
 	@State var scale: CGFloat = 1
+	var onTapComplete: (() -> Void)
 
 	var body: some View {
 		VStack {
 			Image(systemName: "checkmark.circle")
-				.font(.system(size: 64))
+				.font(.system(size: 84))
 				.foregroundColor(AppColors.green)
 				.scaleEffect(scale)
 
 			Text("Great job!")
+
+			Text("Complete")
+				.frame(maxWidth: .infinity)
+				.padding(.vertical, 20)
+				.background(AppColors.green)
+				.foregroundColor(.white)
+				.cornerRadius(100)
+				.padding(.top, 250)
+				.onTapGesture {
+					onTapComplete()
+				}
 		}
+		.padding(.horizontal)
+		.padding(.vertical)
+		.frame(maxHeight: .infinity, alignment: .bottom)
 		.onAppear {
 			let animation = Animation.easeInOut(duration: 0.3).repeatCount(1, autoreverses: true)
 			withAnimation(animation) {
@@ -31,11 +46,13 @@ struct CompletedMeditation: View {
 				}
 			}
 		}
+
+
 	}
 }
 
 struct CompletedMeditation_Previews: PreviewProvider {
 	static var previews: some View {
-		CompletedMeditation()
+		CompletedMeditation(onTapComplete: {})
 	}
 }
