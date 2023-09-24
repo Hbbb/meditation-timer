ac070698cfa40c4b438cc382e1ca047c316ebe59
Harrison Borges <hborges005@gmail.com>
2023-09-23 19:19:46 -0600

    write mindful minutes to healthkit

--- meditation-timer/HealthKitManager.swift
+++ meditation-timer/HealthKitManager.swift
@@ -25,5 +25,26 @@ class HealthKitManager {
 			completion(success, error)
 		}
 	}
+
+	func writeMindfulMinutes(minutes: Double, completion: @escaping (Bool, Error?) -> Void) {
+		guard HKHealthStore.isHealthDataAvailable() else {
+			completion(false, nil)
+			return
+		}
+
+		let mindfulMinutes = HKObjectType.categoryType(forIdentifier: .mindfulSession)!
+
+		// Calculate the start and end times based on the quantity of minutes
+		let endDate = Date()
+		let startDate = Calendar.current.date(byAdding: .minute, value: -Int(minutes), to: endDate)!
+
+		// Create a mindful minutes sample with calculated start and end times
+		let mindfulMinutesSample = HKCategorySample(type: mindfulMinutes, value: 0, start: startDate, end: endDate)
+
+		// Save the mindful minutes sample to HealthKit
+		healthStore.save(mindfulMinutesSample) { success, error in
+			completion(success, error)
+		}
+	}
 }
 
