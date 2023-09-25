07f5fc62a6d391ff108f8be3fd7a95635b5eba17
Harrison Borges <hborges005@gmail.com>
2023-09-25 15:27:19 -0600

    create welcome screen

---
+++ meditation-timer/WelcomeScreen.swift
@@ -0,0 +1,43 @@
+//
+//  WelcomeScreen.swift
+//  meditation-timer
+//
+//  Created by Harrison Borges on 9/25/23.
+//
+
+import SwiftUI
+
+struct WelcomeScreen: View {
+	@EnvironmentObject var vm: MeditationViewModel
+
+	var body: some View {
+		VStack {
+			Image(systemName: "figure.mind.and.body")
+				.font(.system(size: 48))
+				.padding(.bottom, 20)
+
+			Text("Meditation Timer uses HealthKit to read and write mindful minutes to the Health app so you can track your progress as you meditate.")
+				.padding(.bottom, 200)
+
+			PrimaryActionButton(text: "Grant access to Health", onTap: {
+				HealthKitManager.shared.requestHealthKitPermission() { succeeded, err in
+					DispatchQueue.main.async {
+						vm.didCompleteOnboarding()
+					}
+				}
+			})
+
+			Text("Skip")
+				.foregroundStyle(.gray)
+				.onTapGesture {
+					vm.didCompleteOnboarding()
+				}
+		}
+		.padding(30)
+		.frame(maxHeight: .infinity, alignment: .bottom)
+	}
+}
+
+#Preview {
+	WelcomeScreen()
+}
